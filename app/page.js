aaac8a033bb01fd5ea48834e2a8dc775c5c041a5
Codngwthubaid <codngwthubaid@gmail.com>
2024-11-15 13:00:44 +0530

    Working on about Section

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import About from "@/components/About";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
@@ -5,9 +6,12 @@ export default function Home() {
     <>
       <Navbar />
       <section className="container mx-auto border">
-        <div className="text-center text-slate-500 my-24">
+        <div className="text-center text-slate-500 my-32">
           <h1 className="text-[#0096c7] my-6">Transforming Healthcare with AI-Powered Solutions</h1>
           <p className="text-xl">Empower healthcare providers like Ravi with our innovative mobile health platform. Seamlessly manage patient care across locations, track chronic conditions, and access real-time data. Our AI-driven insights enhance decision-making, while features like remote monitoring and predictive analytics ensure proactive care, improving patient outcomes and streamlining workflows.</p></div>
+      </section>
+      <section className="container mx-auto border">
+        <About/>
       </section>
     </>
   );
---
+++ components/About.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import LogoHC from "@/public/LogoHC.jpg"
+import Image from 'next/image'
+
+const About = () => {
+    return (
+        <>
+            <div className='flex justify-around items-center'>
+                <div className='w-[45vw] text-start text-slate-500'>
+                    <h2 className="text-[#0096c7] my-6 text-center font-semibold">About Our AI-Powered Mobile Health Platform</h2>
+                    <div>
+                        <p>Our AI-powered mobile health platform is designed to help healthcare providers like Ravi efficiently manage patient care across multiple locations. By streamlining the tracking of chronic conditions, our solution allows for easy input and retrieval of vital patient data. With features such as remote monitoring, healthcare providers can keep a close eye on patients' health, regardless of their location.
+                        </p>
+                        <p>
+                            The platform also utilizes predictive analytics to forecast potential health deteriorations, ensuring timely interventions and better patient outcomes. Automated alerts notify providers of necessary follow-ups, enabling swift action. Our mission is to transform healthcare delivery by enhancing efficiency and improving the quality of care through innovative AI technology.</p>
+                    </div>
+                </div>
+                <div>
+                    <Image
+                        className="w-[45vw] rounded-2xl"
+                        src={LogoHC}
+                        alt="Health-Connect-Logo"
+                    />
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default About
