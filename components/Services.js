3ae214a5250d599ebfaf513bfccf7a5de86186fe
Codngwthubaid <codngwthubaid@gmail.com>
2024-11-15 16:46:39 +0530

    Working on the services section

---
+++ components/Services.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <>
+    <div>
+    <h2 className="text-[#0096c7] my-6 text-center font-semibold">Our Services</h2>
+    </div>
+    </>
+  )
+}
+
+export default Services
