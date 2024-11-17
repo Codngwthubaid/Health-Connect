62944ba5bbe12842d1e55c36fabce683cfbd121b
Codngwthubaid <codngwthubaid@gmail.com>
2024-11-17 08:26:15 +0530

    KidneyTreatment Section is completed

---
+++ components/TreatmentSection/KidneyTreatment.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import Image from 'next/image'
+import Kidney from "@/public/Kidney.jpg"
+import DiseaseTemplate from './DiseaseTemplate'
+
+const KidneyTreatment = (props) => {
+    return (
+        <div href="#" className="flex items-start bg-white border border-gray-200 rounded-xl shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 h-fit">
+            <Image
+                className="object-contain w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
+                src={Kidney}
+                alt="Kidney"
+            />
+            <div className="flex flex-col justify-between p-4 leading-normal">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-[#0096c7] dark:text-white">{props.DiseaseName}</h5>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{props.DiseaseDetails}
+                </p>
+                <DiseaseTemplate
+                    DiseaseSymptoms="Acute Pyelonephritis Symptoms:"
+                    SymptomsFirst="Fever"
+                    SymptomsSecond="Chills"
+                    SymptomsThird="Back or flank pain"
+                    SymptomsFourth="Nausea"
+                    SymptomsFifth="Vomiting"
+                    SymptomsSixth="Frequent and painful urination"
+                    SymptomsSeventh="Cloudy or foul-smelling urine"
+                />
+                <DiseaseTemplate
+                    DiseaseSymptoms="Chronic Pyelonephritis Symptoms:"
+                    SymptomsFirst="Often milder than acute cases but can include pain"
+                    SymptomsSecond="Fever"
+                    SymptomsThird="A general feeling of being unwell"
+                />
+                <DiseaseTemplate
+                    DiseaseSymptoms="Kidney Abscess Symptoms:"
+                    SymptomsFirst="High fever"
+                    SymptomsSecond="Abdominal pain,"
+                    SymptomsThird="Night sweats"
+                    SymptomsFourth="Severe pain in the back or side"
+                />
+                <DiseaseTemplate
+                    DiseaseSymptoms="Emphysematous Pyelonephritis (EPN) Symptoms:"
+                    SymptomsFirst="Severe abdominal pain "
+                    SymptomsSecond="Fever"
+                    SymptomsThird="Confusion"
+                    SymptomsFourth="Rapid deterioration"
+                />
+                <DiseaseTemplate
+                    DiseaseSymptoms=" Renal Tuberculosis Symptoms:"
+                    SymptomsFirst="Blood in the urinefever"
+                    SymptomsSecond="Chronic back pain"
+                    SymptomsThird="Fever"
+                    SymptomsFourth="weight loss"
+                />
+            </div>
+        </div>
+    )
+}
+
+export default KidneyTreatment
--- components/Treatments.js
+++ components/Treatments.js
@@ -3,6 +3,7 @@ import TypingAnimation from './ui/typing-animation'
 import NoseTreatment from "./TreatmentSection/NoseTreatment"
 import EyeTreatment from './TreatmentSection/EyeTreatment'
 import HeartTreatment from "./TreatmentSection/HeartTreatment"
+import KidneyTreatment from './TreatmentSection/KidneyTreatment'
 
 const Treatments = () => {
     return (
@@ -15,7 +16,7 @@ const Treatments = () => {
                     /></div>
                 <p className='text-slate-500 text-lg text-center mx-10'>At HealthConnect,Enhancing Diagnostic Accuracy, Artificial Intelligence (AI) significantly improves diagnostic accuracy by analyzing vast amounts of medical data and identifying patterns that may be overlooked by human clinicians. For instance, AI algorithms can interpret medical imaging</p>
             </div>
-            <div className='my-24 flex flex-wrap gap-y-5 items-center justify-around'>
+            <div className='my-24 flex flex-wrap gap-y-5 items-start justify-around'>
                 <NoseTreatment
                     DiseaseName="Nose"
                     DiseaseDetails="Nose infections can vary in severity and cause, but they generally lead to symptoms affecting the nose, sinuses, or surrounding areas. Here are some common types of nose infections and their symptoms:"
@@ -27,6 +28,10 @@ const Treatments = () => {
                 <HeartTreatment
                     DiseaseName="Heart"
                     DiseaseDetails="Heart diseases encompass a variety of conditions affecting the heart, ranging from structural problems to electrical disturbances. Here are some common heart diseases and their associated symptoms:"
+                />
+                <KidneyTreatment
+                    DiseaseName="Kidney"
+                    DiseaseDetails="Kidney infections, also known as pyelonephritis, are serious infections that typically start in the bladder or urethra and travel up to one or both kidneys. Here are some notable diseases and conditions associated with or caused by kidney infections"
                 />
             </div>
         </>
